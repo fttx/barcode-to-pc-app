e3ddfb335e6739110e86d9bcf3a6504b5636ca39
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2016-11-20 01:11:36 +0100

    Fix iOS status bar text color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,6 @@ export class MyApp {
       platform.ready().then(() => {
         // Okay, so the platform is ready and our plugins are available.
         // Here you can do any higher level native things you might need.
-        StatusBar.styleDefault();
         StatusBar.overlaysWebView(true);
         StatusBar.backgroundColorByHexString('#B71C1C');
       })
