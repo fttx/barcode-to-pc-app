8ef7ff6bd3cf4e014f5dd22416655b57c5772e13
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2017-05-01 19:00:38 +0200

    Updated ionic to 3.1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { SettingsPage } from '../pages/settings/settings';
@@ -42,6 +44,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
     SettingsPage,
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp),
     MomentModule,
