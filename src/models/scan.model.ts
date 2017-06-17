3c986dbed9deb6edcdc5e4dcb3739268f28716cd
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2017-06-17 19:56:09 +0200

    Fixed type error

--- src/models/scan.model.ts
+++ src/models/scan.model.ts
@@ -1,5 +1,8 @@
-export interface ScanModel {
-    id: string;
+import { BarcodeScanResult } from "@ionic-native/barcode-scanner";
+
+export class ScanModel implements BarcodeScanResult {
+    format: "QR_CODE" | "DATA_MATRIX" | "UPC_E" | "UPC_A" | "EAN_8" | "EAN_13" | "CODE_128" | "CODE_39" | "CODE_93" | "CODABAR" | "ITF" | "RSS14" | "RSS_EXPANDED" | "PDF417" | "AZTEC" | "MSI";
+    cancelled: boolean;
     text: string;
-    format: string;
+    id: string;
 }
