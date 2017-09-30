ad0fdd9f7acd726be30877db41a744b187369ddf
filippo <filippo.tortomasi@gmail.com>
2017-09-30 12:17:07 +0200

    Fix edit scan session name and date

--- src/pages/scan-session/edit-scan-session/edit-scan-session.ts
+++ src/pages/scan-session/edit-scan-session/edit-scan-session.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
 import { ScanSessionModel } from '../../../models/scan-session.model'
+import { requestModelUpdateScanSession } from '../../../models/request.model';
+import { ServerProvider } from '../../../providers/server';
 
 /*
   Generated class for the EditScanSession page.
@@ -14,22 +16,36 @@ import { ScanSessionModel } from '../../../models/scan-session.model'
 })
 export class EditScanSessionPage {
   public scanSession: ScanSessionModel;
+  public date: string;
+  private static timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
 
   constructor(
     public viewCtrl: ViewController,
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    private serverProvider: ServerProvider,
   ) {
     this.scanSession = viewCtrl.getNavParams().data;
+    this.date = (new Date(this.scanSession.date.getTime() - EditScanSessionPage.timezoneOffset)).toISOString().slice(0, -1);
   }
 
   ionViewDidLoad() { }
 
-  onDateChanged(date) {
-    // TODO: ionic è ancora in beta!
-  }
-
   dismiss() {
+    let newDate = this.getDate();
+
+    let request = new requestModelUpdateScanSession().fromObject({
+      scanSessionId: this.scanSession.id,
+      scanSessionName: this.scanSession.name,
+      scanSessionDate: newDate
+    });
+    this.serverProvider.send(request);
+
+    this.scanSession.date = newDate
     this.viewCtrl.dismiss(this.scanSession);
   }
 
+  getDate() {
+    let d = new Date(Date.parse(this.date)).getTime();
+    return (new Date(d + EditScanSessionPage.timezoneOffset));
+  }
 }
