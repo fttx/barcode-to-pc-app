3513e979c042f397399c4a729c4dc22af31da404
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2018-09-08 16:09:47 +0200

    Replace market plugin with launch review plugin, fixes #69

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-// Modules
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { AppVersion } from '@ionic-native/app-version';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Device } from '@ionic-native/device';
 import { GoogleAnalytics } from '@ionic-native/google-analytics';
 import { InAppPurchase2 } from '@ionic-native/in-app-purchase-2';
-import { Market } from '@ionic-native/market';
+import { LaunchReview } from '@ionic-native/launch-review';
 import { NativeAudio } from '@ionic-native/native-audio';
 import { Network } from '@ionic-native/network';
 import { SocialSharing } from '@ionic-native/social-sharing';
@@ -17,9 +16,11 @@ import { Zeroconf } from '@ionic-native/zeroconf';
 import { IonicStorageModule } from '@ionic/storage';
 import { MomentModule } from 'angular2-moment';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { CircleTextComponent } from '../components/circle-text';
 import { AboutPage } from '../pages/about/about';
+import { ArchivedPage } from '../pages/archived/archived';
 import { HelpPage } from '../pages/help/help';
 import { EditScanSessionPage } from '../pages/scan-session/edit-scan-session/edit-scan-session';
 import { ScanSessionPage } from '../pages/scan-session/scan-session';
@@ -29,14 +30,14 @@ import { SelectServerPage } from '../pages/select-server/select-server';
 import { SettingsPage } from '../pages/settings/settings';
 import { WelcomePage } from '../pages/welcome/welcome';
 import { CameraScannerProvider } from '../providers/camera-scanner';
+import { LastToastProvider } from '../providers/last-toast/last-toast';
 import { ScanSessionsStorage } from '../providers/scan-sessions-storage';
 import { ServerProvider } from '../providers/server';
 import { Settings } from '../providers/settings';
 import { Utils } from '../providers/utils';
 import { MyApp } from './app.component';
-import { LastToastProvider } from '../providers/last-toast/last-toast';
-import { ArchivedPage } from '../pages/archived/archived';
-import { MarkdownModule } from 'ngx-markdown';
+
+// Modules
 // Ionic-native
 // Pages
 // import { BuyProPage } from '../pages/buy-pro/buy-pro';
@@ -89,7 +90,7 @@ import { MarkdownModule } from 'ngx-markdown';
     GoogleAnalytics,
     StatusBar,
     SplashScreen,
-    Market,
+    LaunchReview,
     Device,
     SocialSharing,
     AppVersion,
