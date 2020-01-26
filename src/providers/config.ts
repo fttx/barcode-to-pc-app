5c5047db55b9c5d47471af95fd8d1e05edcea818
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2020-01-26 16:15:03 +0100

    Remove unused constants

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -18,13 +18,11 @@ export class Config {
   public static SERVER_PORT = 57891;
   public static WEBSITE_NAME = 'barcodetopc.com';
   public static EMAIL_SUPPORT = 'support@barcodetopc.com';
-  public static EMAIL_ORDERS = 'orders@fastspring.com';
   public static DEFAULT_CONTINUE_MODE_TIMEOUT = 0;
   public static NO_RUNNINGS_BEFORE_SHOW_RATING = 5;
   public static NO_RUNNINGS_BEFORE_SHOW_SOUND_FEEDBACK_OR_DIALOG = 2;
   public static SHOW_CANNOT_FIND_DIALOG_TIMEOUT = 90 * 1000;
   public static DEFAULT_REPEAT_INVERVAL = 500;
-  public static GOOGLE_ANALYTICS_ID = 'UA-87867313-1';
   public static DEFAULT_ACQUISITION_LABEL = "Place a barcode inside the scan area.\nPress the back button to exit.";
 
   constructor() {
