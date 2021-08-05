5933006af0aab0ab398ced7d3bd9b3af8cbed42e
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2021-08-05 12:39:51 +0200

    Fix default language file loading issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,9 +57,9 @@ export class MyApp {
 
       this.firebaseAnalytics.setEnabled(!Config.DEBUG);
 
-      this.translate.setDefaultLang('en');
-      if (this.translate.getBrowserLang() !== undefined) {
-        this.translate.use(this.translate.getBrowserLang());
+      const lang = this.translate.getBrowserLang();
+      if (lang !== undefined && this.translate.getLangs().indexOf(lang) != -1) {
+        this.translate.use(lang);
       } else {
         this.translate.use('en');
       }
