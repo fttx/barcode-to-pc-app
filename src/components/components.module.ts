c85cf429b3edf41d3037443bc5b531002e91bebf
Filippo <filippo.tortomasi@gmail.com>
2024-07-27 13:20:57 +0200

    fix: build error

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BtpToastComponent } from './btp-toast/btp-toast';
-@NgModule({
-	declarations: [BtpToastComponent],
-	imports: [],
-	exports: [BtpToastComponent]
-})
-export class ComponentsModule {}
