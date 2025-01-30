bf26d8f46dd1ebd301421df11440bbca5837bf80
Filippo <filippo.tortomasi@gmail.com>
2025-01-30 16:14:19 +0100

    feat: add image-view back button support

--- src/pages/image-viewer/image-viewer.ts
+++ src/pages/image-viewer/image-viewer.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { NavParams, ViewController } from 'ionic-angular';
+import { NavParams, ViewController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'image-viewer',
@@ -9,15 +9,29 @@ import { NavParams, ViewController } from 'ionic-angular';
 export class ImageViewerPage {
   public imageBase64: string;
   public safeImageUrl: SafeUrl;
+  private unregisterBackButton: any;
 
   constructor(
     private navParams: NavParams,
     private viewCtrl: ViewController,
     private sanitizer: DomSanitizer,
+    private platform: Platform,
   ) {
     // Retrieve the base64 image string passed by the modal
     this.imageBase64 = this.navParams.get('imageBase64');
     this.safeImageUrl = this.sanitizer.bypassSecurityTrustUrl(this.imageBase64);
+  }
+
+  ionViewDidEnter() {
+    this.unregisterBackButton = this.platform.registerBackButtonAction(() => {
+      this.closeModal();
+    });
+  }
+
+  ionViewWillLeave() {
+    if (this.unregisterBackButton) {
+      this.unregisterBackButton();
+    }
   }
 
   closeModal() {
